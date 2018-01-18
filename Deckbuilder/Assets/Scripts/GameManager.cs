f46fb10076c170a39603d4c9ae61cde938cc57ff
NickAzn <nicholaslam16@gmail.com>
2018-01-18 00:36:46 -0800

    Unit Attacking

---
+++ Deckbuilder/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public SpotStats[] playerSpots;
+	public SpotStats[] enemySpots;
+
+	public void endTurn() {
+		for (int i = 0; i < playerSpots.Length; i++) {
+			if (playerSpots [i].hasUnit) {
+				if (playerSpots [i].collumn == 1) {
+					if (enemySpots [i].hasUnit) {
+						playerSpots [i].attack (enemySpots [i]);
+					} else if (enemySpots [i + 3].hasUnit) {
+						playerSpots [i].attack (enemySpots [i + 3]);
+					}
+				} else {
+					if (enemySpots [i - 3].hasUnit) {
+						playerSpots [i].attack (enemySpots [i - 3]);
+					} else if (enemySpots [i].hasUnit) {
+						playerSpots [i].attack (enemySpots [i]);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Deckbuilder/Assets/Scripts/SpotStats.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotStats : MonoBehaviour {
+
+	public bool hasUnit = false;
+	public int row = 0;
+	public int collumn = 0;
+	public int health = 0;
+	public int damage = 0;
+
+	public bool addUnit(int health, int damage) {
+		if (!hasUnit) {
+			health = this.health;
+			damage = this.damage;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void attack(SpotStats enemy) {
+		enemy.takeDamage (damage);
+	}
+
+	public void takeDamage(int amount) {
+		health -= amount;
+		if (health <= 0) {
+			hasUnit = false;
+			health = 0;
+			damage = 0;
+		}
+	}
+}
